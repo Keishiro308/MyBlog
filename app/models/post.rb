40c1788303581d0cd3c2e8c95d52e9b4836ef65e
Keishiro <kn11308@gmail.com>
2019-10-01 23:58:48 +0900

    Add action text

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,9 @@
 class Post < ApplicationRecord
   mount_uploader :image_name, PostImageUploader
+  has_rich_text :content
 
-  
+  validates :user_id, presence: true
+  validates :title, presence: true
+
+
 end
