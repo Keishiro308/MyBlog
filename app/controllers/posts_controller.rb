7ee4cd78c701f3433b5691d450b021234c347710
Keishiro <kn11308@gmail.com>
2019-10-12 00:03:26 +0900

    Improve tags

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :set_search_post, only: [:show, :edit, :update, :destroy,:tag]
+  before_action :set_search_post
   PER=8
 
   # GET /posts
